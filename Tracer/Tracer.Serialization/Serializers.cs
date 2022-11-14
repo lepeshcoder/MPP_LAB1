06c14c6990837edac3dd5e88d34d23ae7b37b627
lepeshcoder <nelepesh100@mail.ru>
2022-11-14 18:35:21 +0300

    add plugins loading and serialize

--- Tracer/Tracer.Serialization/Serializers.cs
+++ Tracer/Tracer.Serialization/Serializers.cs
@@ -1,7 +1,63 @@
-﻿namespace Tracer.Serialization
+﻿using System.Reflection;
+using Tracer.Serialization.Abstractions;
+
+namespace Tracer.Serialization
 {
-    public class Class1
+    public static class Serializers
     {
+        public static List<ITraceResultSerializer> GetSerializers(string path)
+        {
+            List<ITraceResultSerializer> serializers = new();
+            try
+            {
+                DirectoryInfo pluginDirectory = new DirectoryInfo(path);
+                var pluginFiles = Directory.GetFiles(path, "*.dll");
+                foreach (var file in pluginFiles)
+                {
+                    try
+                    {
+                        Assembly assembly = Assembly.LoadFrom(file);
+                        var types = assembly.GetTypes().Where(x => x.GetInterfaces().Contains(typeof(Tracer.Serialization.Abstractions.ITraceResultSerializer)));
+                        foreach (var type in types)
+                        {
+                            try
+                            {
+                                ITraceResultSerializer? serializer = assembly.CreateInstance(type.FullName!) as ITraceResultSerializer;
+                                serializers.Add(serializer!);
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine(ex);
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return serializers;
+        }
+
+        public static void SerializeIntoFiles(Core.TraceResult traceResult, String serializersFolderPath, String resultFolder)
+        {
+            if (!Directory.Exists(resultFolder))
+            {
+                Directory.CreateDirectory(resultFolder);
+            }
+            List<ITraceResultSerializer> serializers = Serializers.GetSerializers(serializersFolderPath);
+            foreach (var serializer in serializers)
+            {
+                using var fileStream = new FileStream(Path.Combine(resultFolder, $"result.{serializer.Format}"), FileMode.Create);
+                serializer.Serialize(traceResult, fileStream);
+            }
+        }
+
 
     }
 }
