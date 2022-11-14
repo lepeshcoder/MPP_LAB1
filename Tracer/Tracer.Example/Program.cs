aed5b0de2f3318f0bf276b8f03b89b5e4360c1a9
lepeshcoder <nelepesh100@mail.ru>
2022-11-14 20:38:24 +0300

    add example and test it

--- Tracer/Tracer.Example/Program.cs
+++ Tracer/Tracer.Example/Program.cs
@@ -1,2 +1,90 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tracer.Core;
+using Tracer.Serialization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Tracer.Core.Tracer tracer = new();
+        Foo foo = new(tracer);
+        Pup pup = new(tracer);
+
+        var t1 = new Thread(() =>
+        {
+            foo.MyMethod();
+        });
+
+        var t2 = new Thread(() =>
+        {
+            pup.PupMethod();
+        });
+
+        t1.Start();
+        t2.Start();
+
+        t1.Join();
+        t2.Join();
+
+        var traceResult = tracer.GetTraceResult();
+
+        Serializers.SerializeIntoFiles(traceResult, "Plugins", Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Results"));
+    }
+}
+
+public class Foo
+{
+    private Bar _bar;
+
+    private Pup _pup;
+    private ITracer _tracer;
+
+    internal Foo(ITracer tracer)
+    {
+        _tracer = tracer;
+        _bar = new Bar(_tracer);
+        _pup = new Pup(_tracer);
+    }
+
+    public void MyMethod()
+    {
+        _tracer.StartTrace();
+        Thread.Sleep(50);
+        _bar.InnerMethod();
+        _pup.PupMethod();
+        _tracer.StopTrace();
+    }
+}
+
+public class Bar
+{
+    private ITracer _tracer;
+
+    internal Bar(ITracer tracer)
+    {
+        _tracer = tracer;
+    }
+
+    public void InnerMethod()
+    {
+        _tracer.StartTrace();
+        Thread.Sleep(100);
+        _tracer.StopTrace();
+    }
+}
+
+public class Pup
+{
+    private ITracer _tracer;
+
+    internal Pup(ITracer tracer)
+    {
+        _tracer = tracer;
+    }
+
+    public void PupMethod()
+    {
+        _tracer.StartTrace();
+        Thread.Sleep(50);
+        _tracer.StopTrace();
+    }
+}
