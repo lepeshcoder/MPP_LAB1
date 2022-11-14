f913240c3e2e667b4ab350e69fe50807c6e5757c
lepeshcoder <nelepesh100@mail.ru>
2022-11-14 20:22:39 +0300

    finally add Tracer Functionality

--- Tracer/Tracer.Core/Tracer.cs
+++ Tracer/Tracer.Core/Tracer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,19 +10,54 @@ namespace Tracer.Core
 {
     public class Tracer : ITracer
     {
+        private ConcurrentDictionary<int, ThreadInfo> _threads = new();
+
         public TraceResult GetTraceResult()
         {
-            throw new NotImplementedException();
+            Dictionary<int, ThreadInfo> traceResult = new();
+            foreach (var thread in _threads)
+                traceResult.Add(thread.Key, thread.Value);
+            return new TraceResult(traceResult);
         }
 
         public void StartTrace()
         {
-            throw new NotImplementedException();
+            int threadId = Environment.CurrentManagedThreadId;
+
+            var threadInfo = _threads.GetOrAdd(threadId, new ThreadInfo());
+
+            StackTrace stackTrace = new();
+
+            var stackTraceInfo = stackTrace.GetFrame(1)!.GetMethod();
+            String methodName = stackTraceInfo!.Name;
+            String className = stackTraceInfo.DeclaringType!.Name;
+
+            Stopwatch stopwatch = new();
+            var methodInfo = new MethodInfo(methodName, className, stopwatch);
+
+            if (threadInfo.RunningMethods.Count != 0)
+            {
+                var parentMethod = threadInfo.RunningMethods.First();
+                parentMethod.InnerMethods.Add(methodInfo);
+            }
+            else
+            {
+                threadInfo.Methods.Add(methodInfo);
+            }
+            threadInfo.RunningMethods.Push(methodInfo);
+
+            stopwatch.Start();
+
         }
 
         public void StopTrace()
         {
-            throw new NotImplementedException();
+            int threadId = Environment.CurrentManagedThreadId;
+
+            MethodInfo? methodInfo;
+            if (!_threads[threadId].RunningMethods.TryPop(out methodInfo)) return;
+
+            methodInfo.StopWatch.Stop();
         }
     }
 }
